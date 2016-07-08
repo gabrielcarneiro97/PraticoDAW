e765df0cb3427ba619f3470d70312ea437934548
Alan Borges Martins Bispo <sr.alan.bispo@gmail.com>
2016-07-08 09:55:35 -0300

    Adição de comentários e organização do código da classe CandidatoDAO.

--- backend/src/classes/CandidatoDAO.php
+++ backend/src/classes/CandidatoDAO.php
@@ -5,6 +5,23 @@ class CandidatoDAO implements DefaultDAO{
   private function __construct(){
   }
 
+/* Função que instancia a classe DAO */
+  public static function getInstance() {
+    static $instance = null;
+    if (null === $instance) {
+        $instance = new static();
+    }
+    return $instance;
+  }
+
+//--************************************************************************--//
+//--*************Inicio dos métodos de persistência e cadastro**************--//
+//--************************************************************************--//
+
+  /*
+  *  Função que faz a persistência dos dados de login dos usuários dentro do
+  *  diretório src/private/logs/ no arquivo login.json.
+  */
   private function cadastra($login,$senha){
     $jsonToPrint = array( 'login' => $login,
                           'senha' => $senha);
@@ -25,6 +42,10 @@ class CandidatoDAO implements DefaultDAO{
     fclose($newFile);
   }
 
+  /*
+  *  Função que faz a persistência dos dados do usuário
+  *  no diretório src\private\userdata com o nome userdata-{id do usuário}.json
+  */
   private function insertData($pNome,$sNome,$sex,$cidade,$estado,$pais,$id){
     $jsonToPrint = array( 'pNome' => $pNome,
                           'sNome' => $sNome,
@@ -37,19 +58,102 @@ class CandidatoDAO implements DefaultDAO{
     json_encode($jsonToPrint);
     $newJson[] = $jsonToPrint;
 
-    $newFile = fopen('C:\Users\srala\Desktop\Info\DAW\2trim\PraticoDAW\backend\src\private\data'.'\\'.$id.'.json', "w") or die("Unable to open file!");
+    $newFile = fopen('C:\Users\srala\Desktop\Info\DAW\2trim\PraticoDAW\backend\src\private\userdata'.'\userdata-'.$id.'.json', "w") or die("Unable to open file!");
     fwrite($newFile, json_encode($newJson));
     fclose($newFile);
   }
 
-  public static function getInstance() {
-    static $instance = null;
-    if (null === $instance) {
-        $instance = new static();
+  /*
+  *  Função padrão de inserção do usuário no sistema.
+  */
+  public function insert($array){
+    $novoCandidato = new Candidato($array);
+    $novoCandidato->setId($this->getIdToUser());
+    $this->cadastra($novoCandidato->getLogin(),$novoCandidato->getSenha());
+    $this->insertData($novoCandidato->getPrimeiroNome(),
+                      $novoCandidato->getSobreNome(),
+                      $novoCandidato->getTipoSexo(),
+                      $novoCandidato->getCidade(),
+                      $novoCandidato->getEstado(),
+                      $novoCandidato->getPais(),
+                      $novoCandidato->getId()
+                      );
+    return $novoCandidato;
+  }
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+//--************************************************************************--//
+//--***********Início dos métodos para a exclusão dos usuários**************--//
+//--************************************************************************--//
+
+  /*
+  *  Função padrão de deletar um usuário específico do sistema.
+  */
+  public function delete($object){
+    if ($_SESSION["candidatos"][$object->id]){
+      unset($_SESSION["candidatos"][$object->id]);
+      return true; //funcionou a exclusão
     }
-    return $instance;
+    return false; //não rolou a exclusão pq o trem não existia!
   }
 
+  /*
+  *  Função padrão de deletar todos os usuários do sistema.
+  */
+  public function deleteAll() {
+    $_SESSION["candidatos"] = [];
+  }
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+//--************************************************************************--//
+//--*******Início dos métodos para a atualização de dados dos usuários******--//
+//--************************************************************************--//
+
+  /*
+  *  Função padrão de atualização dos usuários do sistema.
+  */
+  public function update($object) {
+    $candidato = $_SESSION["candidatos"][$object->id];
+
+    if($candidato){
+      $candidato->login = $object->login;
+      $candidato->senha = $object->senha;
+      $candidato->nomeCompleto = $object->nomeCompleto;
+      return true;
+    }
+    return false;
+  }
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+//--************************************************************************--//
+//--*******Início dos métodos para a atualização de dados dos usuários******--//
+//--************************************************************************--//
+
+  /*
+  *  Função padrão que retorna um usuário em específico
+  *  caçando pelo id dele no sistema.
+  */
+  public function getById($id) {
+    return $_SESSION["candidatos"][$id];
+  }
+
+  /*
+  *  Função padrão que retorna todos os usuário do sistema.
+  */
+  public function getAll(){
+    return $_SESSION["candidatos"];
+  }
+}
+
+  /*
+  *  Função padrão que retorna a quantidade de usuários listados no login.json.
+  */
   private function getIdToUser(){
     $file = fopen('C:\Users\srala\Desktop\Info\DAW\2trim\PraticoDAW\backend\src\private\logs\login.json', "r") or die("Unable to proceed!");
     $jsonStr = "";
@@ -66,62 +170,3 @@ class CandidatoDAO implements DefaultDAO{
 
     return $varCount;
   }
-
-  public function insert($array){
-    $novoCandidato = new Candidato($array);
-    $novoCandidato->setId($this->getIdToUser());
-    $this->cadastra($novoCandidato->getLogin(),$novoCandidato->getSenha());
-    $this->insertData($novoCandidato->getPrimeiroNome(),
-                      $novoCandidato->getSobreNome(),
-                      $novoCandidato->getTipoSexo(),
-                      $novoCandidato->getCidade(),
-                      $novoCandidato->getEstado(),
-                      $novoCandidato->getPais(),
-                      $novoCandidato->getId()
-                      );
-    return $novoCandidato;
-  }
-
-
-  public function delete($object){
-    if ($_SESSION["candidatos"][$object->id]){
-      unset($_SESSION["candidatos"][$object->id]);
-      return true; //funcionou a exclusão
-    }
-    return false; //não rolou a exclusão pq o trem não existia!
-  }
-
-
-  public function deleteAll() {
-    $_SESSION["candidatos"] = [];
-  }
-
-
-  public function update($object) {
-    $candidato = $_SESSION["candidatos"][$object->id];
-
-    if($candidato){
-      $candidato->login = $object->login;
-      $candidato->senha = $object->senha;
-      $candidato->nomeCompleto = $object->nomeCompleto;
-      return true;
-    }
-
-    return false;
-  }
-
-  public function getById($id) {
-    return $_SESSION["candidatos"][$id];
-  }
-
-  public function getBy($data){
-    return array_filter($_SESSION["candidatos"],function($var){
-      return ($var->getId() == $data['id'] || $data['id'] === NULL);
-    });
-  }
-
-
-  public function getAll() {
-    return $_SESSION["candidatos"];
-  }
-}
